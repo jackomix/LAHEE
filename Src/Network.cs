f4f3059ea897386aaabf2d2326dd00f8f31e77ec
jackomix <jacklack33@outlook.com>
2026-04-23 01:13:17 -0400

    Fix case-sensitivity compiler error in RAPing function

--- Src/Network.cs
+++ Src/Network.cs
@@ -407,7 +407,7 @@ static class Routes {
         //int hardcoreFlag = Int32.Parse(ctx.Request.GetParameter("h")); // 1/0
         // int secondsSinceUnlock = ctx.Request.GetParameter("o");
 
-        GameData game = StaticDataManager.FindGameDataByHash(gamehash);
+        GameData game = StaticDataManager.FindGameDataByHash(gameHash);
         if (game == null) {
             Log.User.LogWarning("ROM Hash {hash} not registered!", gameHash);
             await ctx.Response.SendJson(new RAErrorResponse("ROM hash is not registered!"));
