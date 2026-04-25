91ded1fd3789a32a322ac5b7b2ec424a9b2b355a
jackomix <jacklack33@outlook.com>
2026-04-25 16:01:50 -0400

    Add amber-es and LAHEE source files

---
+++ LAHEE/Haruka.Common/Configuration/AppConfig.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Haruka.Common.Configuration;
+
+public class AppConfig {
+    public static AppConfig Primary { get; private set; }
+
+    private readonly IConfigurationRoot config;
+
+    public static void Initialize() {
+        Primary = new AppConfig("Haruka.Common.Settings");
+    }
+
+    public AppConfig(string prefix) {
+        config = new ConfigurationBuilder()
+            .AddJsonFile(prefix + ".json", false)
+            .AddJsonFile(prefix + ".debug.json", true, true)
+            .AddJsonFile(prefix + ".local.json", true, true)
+            .Build();
+    }
+
+    public string Get(string section, string value) {
+        return config.GetSection(section).GetSection(value)?.Value;
+    }
+
+    public string Get(string section, string subsection, string value) {
+        return config.GetSection(section).GetSection(subsection).GetSection(value).Value;
+    }
+
+    public int GetInt(string section, string value) {
+        return config.GetSection(section).GetValue<int>(value);
+    }
+
+    public int GetInt(string section, string subsection, string value) {
+        return config.GetSection(section).GetSection(subsection).GetValue<int>(value);
+    }
+
+    public bool GetBool(string section, string value) {
+        return config.GetSection(section).GetValue<bool>(value);
+    }
+
+    public bool GetBool(string section, string subsection, string value) {
+        return config.GetSection(section).GetSection(subsection).GetValue<bool>(value);
+    }
+
+    public IConfigurationSection GetSection(string section) {
+        return config.GetSection(section);
+    }
+}
---
+++ LAHEE/Src/Util/Extensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Web;
+using Newtonsoft.Json;
+using WatsonWebserver.Core;
+
+namespace LAHEE.Util;
+
+static class Extensions {
+    public static string GetParameter(this HttpRequestBase req, string str) {
+        return HttpUtility.ParseQueryString(req.DataAsString)[str];
+    }
+
+    public static async Task SendJson(this HttpResponseBase resp, object obj) {
+        string data = JsonConvert.SerializeObject(obj);
+        await resp.Send(data);
+    }
+
+    public static IEnumerable<Exception> GetInnerExceptions(this Exception ex) {
+        if (ex == null) {
+            throw new ArgumentNullException(nameof(ex));
+        }
+
+        Exception innerException = ex;
+        do {
+            yield return innerException;
+            innerException = innerException.InnerException;
+        } while (innerException != null);
+    }
+}
